577dd5e83bb13cde6d616a992c1c01573fbfb45d
Sidhartha D <144550797+dsid271@users.noreply.github.com>
2024-10-14 17:11:48 +0530

    Update __init__.py

--- src/skill_modules/__init__.py
+++ src/skill_modules/__init__.py
@@ -1,5 +1,6 @@
+from .skill_base import SkillBase
 from .skill_router import SkillRouter
 from .qa_skill import QASkill
 from .summarization_skill import SummarizationSkill
 
-__all__ = ['SkillRouter', 'QASkill', 'SummarizationSkill']
+__all__ = ['SkillBase', 'SkillRouter', 'QASkill', 'SummarizationSkill']
